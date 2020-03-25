48e8bb48c30aff0bf6f9b2160ed069eac2f2e8fc
Vinicius Moraes <43692948+vncmoraes@users.noreply.github.com>
2020-03-25 17:51:49 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ from google.cloud import firestore
 db = firestore.Client()
 
 
-def insert_name(chat_id, name):
-    db.collection('key_owners').document('names').update({str(chat_id): str(name)})
+def insert_name(user_id, name):
+    db.collection('key_owners').document('names').update({str(user_id): str(name)})
 
 
-def delete_name(chat_id):
-    db.collection('key_owners').document('names').update({str(chat_id): firestore.DELETE_FIELD})
+def delete_name(user_id):
+    db.collection('key_owners').document('names').update({str(user_id): firestore.DELETE_FIELD})
 
 
 def get_key_owners():
